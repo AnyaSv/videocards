8f50254bbb4a7417056ea7ae71c16bc34d92c999
Anya <sviridowa545@gmail.com>
2026-03-19 02:46:30 +0300

    Добавил новые видеокарты

--- src/dataset/videocards.js
+++ src/dataset/videocards.js
@@ -1,6 +1,9 @@
 import Ph1 from '@/assets/Ph1.jpg';
 import Ph2 from '@/assets/Ph2.jpg';
 import Ph3 from '@/assets/Ph3.jpg';
+import Ph4 from '@/assets/Ph4.jpg';
+import Ph5 from '@/assets/Ph5.jpg';
+import Ph6 from '@/assets/Ph6.jpg';
 
 const videocards = [
   {
@@ -28,7 +31,7 @@ const videocards = [
     img: Ph2
   },
   {
-    id: 1,
+    id: 3,
     name: 'Просто видеокарта',
     manufacturer: 'LG',
     ram: {
@@ -39,7 +42,42 @@ const videocards = [
     cost: 10,
     img: Ph3
   },
-
+  {
+    id: 4,
+    name: 'Геймерская видеокарта',
+    manufacturer: 'NVIDIA',
+    ram: {
+      value: 8,
+      unit: 'Gb',
+    },
+    color: '00ff00',
+    cost: 300,
+    img: Ph4
+  },
+  {
+    id: 5,
+    name: 'Мощная видеокарта',
+    manufacturer: 'AMD',
+    ram: {
+      value: 6,
+      unit: 'Gb',
+    },
+    color: 'ffff00',
+    cost: 250,
+    img: Ph5
+  },
+  {
+    id: 6,
+    name: 'Эконом видеокарта',
+    manufacturer: 'Intel',
+    ram: {
+      value: 1,
+      unit: 'Gb',
+    },
+    color: 'ff00ff',
+    cost: 80,
+    img: Ph6
+  }
 ];
 
 export { videocards };
