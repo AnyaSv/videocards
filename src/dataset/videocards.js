037c3b7fee1e7b7bd23526e0a5a7deefdb34c676
AnyaSv <sviridowa545@gmail.com>
2026-03-19 04:55:07 +0300

    Сохраняем текущий код перед созданием новой ветки

--- src/dataset/videocards.js
+++ src/dataset/videocards.js
@@ -1,9 +1,7 @@
 import Ph1 from '@/assets/Ph1.jpg';
 import Ph2 from '@/assets/Ph2.jpg';
 import Ph3 from '@/assets/Ph3.jpg';
-import Ph4 from '@/assets/Ph4.jpg';
-import Ph5 from '@/assets/Ph5.jpg';
-import Ph6 from '@/assets/Ph6.jpg';
+
 
 const videocards = [
   {
@@ -41,42 +39,6 @@ const videocards = [
     color: '0000ff',
     cost: 10,
     img: Ph3
-  },
-  {
-    id: 4,
-    name: 'Геймерская видеокарта',
-    manufacturer: 'NVIDIA',
-    ram: {
-      value: 8,
-      unit: 'Gb',
-    },
-    color: '00ff00',
-    cost: 300,
-    img: Ph4
-  },
-  {
-    id: 5,
-    name: 'Мощная видеокарта',
-    manufacturer: 'AMD',
-    ram: {
-      value: 6,
-      unit: 'Gb',
-    },
-    color: 'ffff00',
-    cost: 250,
-    img: Ph5
-  },
-  {
-    id: 6,
-    name: 'Эконом видеокарта',
-    manufacturer: 'Intel',
-    ram: {
-      value: 1,
-      unit: 'Gb',
-    },
-    color: 'ff00ff',
-    cost: 80,
-    img: Ph6
   }
 ];
 
