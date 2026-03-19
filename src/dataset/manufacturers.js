0134beb4caeff6b22a48eef827a7000830f4e057
AnyaSv <sviridowa545@gmail.com>
2026-03-19 05:01:53 +0300

    Описание внесённых изменений

--- src/dataset/manufacturers.js
+++ src/dataset/manufacturers.js
@@ -1,17 +1,10 @@
 const manufacturers = [
-   {
-      id: 1,
-      name: 'LG',
-   },
-   {
-      id: 2,
-      name: 'ASUS',
-   },
-   {
-      id: 3,
-      name: 'Xiaomi',
-   },
-   
+  { id: 1, name: 'LG' },
+  { id: 2, name: 'ASUS' },
+  { id: 3, name: 'Xiaomi' },
+  { id: 4, name: 'NVIDIA' },
+  { id: 5, name: 'AMD' },
+  { id: 6, name: 'Intel' },
 ]
 
 export { manufacturers }
